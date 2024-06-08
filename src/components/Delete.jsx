6ebc1800b635e744c55f7c43fdd3fc23773066ba
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-08 06:52:35 +0530

    user app

---
+++ src/components/Delete.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Delete = () => {
+  return (
+    <div><div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <h1><center>Search</center></h1>
+                <lable className="form-label">User name</lable>
+                <input type="text" className="form-control" />
+                <buttn className="btn btn-success">Search</buttn>
+            </div>
+        </div>
+    </div></div>
+  )
+}
+
+export default Delete
